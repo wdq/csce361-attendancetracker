5211bde9de45d0df0ce7c9dda4341cd219149266
Michael Fay <michaelfay95@Docking_Station.internal>
2017-04-17 15:13:43 -0500

    OTA updates

--- Code/node/rpi/ota.py
+++ Code/node/rpi/ota.py
@@ -1,11 +1,13 @@
 # Ota update file.
 import time
-
+import os
 
 
 def code_update():
 	time.sleep(1)
 	target.write( "Updating code\n" )
+	os.system("git pull")
+	target.write( "Code updated\n" )
 	time.sleep(1)
 	target.write( "Calling config update\n" )
 	config_update()
