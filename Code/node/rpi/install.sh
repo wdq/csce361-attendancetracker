29c48cecba07c98550a2ec48b12f1064f283740c
Michael Fay <michaelfay95@Docking_Station.internal>
2017-04-18 17:06:00 -0500

    Updates to install script

--- Code/node/rpi/install.sh
+++ Code/node/rpi/install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 echo "Installing python componentents"
+# Getting dependencies
 sudo apt install python-setuptools python-dev build-essential 
+sudo get install python-pip python-dev ipython
+sudo get install bluetooth libbluetooth-dev
+
+# Installing PIP
 sudo easy_install pip 
-sudo pip install --upgrade virtualenv 
+
+# Installing packages needed through pip
+sudo pip install pybluez
 pip install pybluez
 pip install websocket-client
+
+# Setting up virtual envierment
+sudo pip install --upgrade virtualenv 
 echo "Done installing components"
