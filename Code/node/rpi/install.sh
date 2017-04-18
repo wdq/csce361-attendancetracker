04ffd861e5d0360b065922d382bd7ec1b2f3451b
Michael Fay <michaelfay95@Docking_Station.internal>
2017-04-18 17:00:10 -0500

    Updates to the install script

--- Code/node/rpi/install.sh
+++ Code/node/rpi/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Installing python componentents"
-sudo apt install pip 
+sudo apt install python-setuptools python-dev build-essential 
+sudo easy_install pip 
+sudo pip install --upgrade virtualenv 
 pip install pybluez
 pip install websocket-client
 echo "Done installing components"
