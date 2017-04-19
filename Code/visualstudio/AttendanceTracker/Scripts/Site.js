700e7ea255b03a215a3c571649a75ed4356726d2
William Quade <liam@quade.co>
2017-04-19 12:15:40 -0500

    Add ajax loading spinner, clean up autocomplete text field style, clean up table pages list style.

---
+++ Code/visualstudio/AttendanceTracker/Scripts/Site.js
@@ -0,0 +1,11 @@
+﻿//var $loading = $('#loadingIcon').hide();
+
+$(document)
+  .ajaxStart(function () {
+      $("div#loadingIcon").addClass('show');
+      //$loading.show();
+  })
+  .ajaxStop(function () {
+      $("div#loadingIcon").removeClass('show');
+      //$loading.hide();
+  });
