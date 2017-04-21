5e4a3201e384efb7e7da1af254c1bbe6d99b10bf
William Quade <liam@quade.co>
2017-04-21 12:17:04 -0500

    Update SQL folder to include database dump.

--- Code/visualstudio/sql/CreateSchemaLog.bat
+++
@@ -1,1 +0,0 @@
-call VersionControl\TSqlMigrations.Console.exe -d AttendanceTracker -cs "Data source=LOCALHOST\SQLEXPRESS;Integrated Security=true" -rs AttendanceTracker\Updates\00.00.0001.sql
--- Code/visualstudio/sql/ScriptOut AttendanceTracker.bat
+++
@@ -1,1 +0,0 @@
-VersionControl\TSqlMigrations.Console.exe -d AttendanceTracker -b AttendanceTracker -cs "Data source=LOCALHOST\SQLEXPRESS;Integrated Security=true" -si -sc
